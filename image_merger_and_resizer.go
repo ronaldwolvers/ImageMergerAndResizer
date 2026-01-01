5d7469655279f6a761037518d9498e0cc6c448b3
Automated redeployment <ahcwolvers@gmail.com>
2026-01-01 17:30:16 +0100

    [Auto-generated commit message]
    ---
    
    Modified:
    `image_merger_and_resizer.go`

--- image_merger_and_resizer.go
+++ image_merger_and_resizer.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"image"
 	"image/gif"
@@ -9,6 +10,7 @@ import (
 	"os"
 	"os/user"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -26,12 +28,31 @@ func main() {
 	fmt.Println("Printing program arguments:")
 	fmt.Println(os.Args)
 
-	if len(os.Args) <= 1 {
-		fmt.Println("Usage: image_merger_and_resizer <image_file>")
+	if len(os.Args) <= 3 || (strings.ToLower(os.Args[2]) != "scale" && strings.ToLower(os.Args[2]) != "merge") {
+		fmt.Println("Usage: image_merger_and_resizer <image_file> <scale|merge> [scale_factor|merge_file] [output_file]")
 		os.Exit(1)
 	}
 
 	imageFile := os.Args[1]
+	programCommand := os.Args[2]
+
+	var scaleFactor = -1
+	var mergeFile = ""
+	var expandedMergeFile = ""
+	var err error
+
+	if programCommand == "scale" {
+		scaleFactor, err = strconv.Atoi(os.Args[3])
+		if err != nil {
+			fmt.Println("Error converting <scale_factor> to a number. Error: ", err.Error())
+			os.Exit(1)
+		}
+	} else if programCommand == "merge" {
+		mergeFile = os.Args[3]
+		currentDir, _ := user.Current()
+		homeDir := currentDir.HomeDir
+		expandedMergeFile = strings.ReplaceAll(mergeFile, "~", homeDir)
+	}
 
 	regexpMatches := compiledRegex.FindSubmatch([]byte(imageFile))
 	if len(regexpMatches) <= 1 {
@@ -58,17 +79,17 @@ func main() {
 		os.Exit(1)
 	}
 
-	var image image.Image
+	var decodedImage image.Image
 	var decodeErr error
 	if strings.ToLower(fileExtension) == "gif" {
 		fmt.Println("Decoding gif...")
-		image, decodeErr = gif.Decode(fileReader)
+		decodedImage, decodeErr = gif.Decode(fileReader)
 	} else if strings.ToLower(fileExtension) == "jpeg" {
 		fmt.Println("Decoding jpeg...")
-		image, decodeErr = jpeg.Decode(fileReader)
+		decodedImage, decodeErr = jpeg.Decode(fileReader)
 	} else if strings.ToLower(fileExtension) == "png" {
 		fmt.Println("Decoding png...")
-		image, decodeErr = png.Decode(fileReader)
+		decodedImage, decodeErr = png.Decode(fileReader)
 	} else {
 		fmt.Println("Unknown file extension:", fileExtension)
 		os.Exit(1)
@@ -78,10 +99,76 @@ func main() {
 		fmt.Println("Error decoding file: ", decodeErr.Error())
 		os.Exit(1)
 	}
-	if image == nil {
+	if decodedImage == nil {
 		fmt.Println("No image was read from the specified file...")
 		os.Exit(1)
 	}
 
-	fmt.Println("This image has color model: ", image.ColorModel())
+	fmt.Println("This image has color model: ", decodedImage.ColorModel())
+
+	var resultImage image.Image
+	var encodeErr error
+	if programCommand == "scale" {
+		resultImage, encodeErr = scaleImage(decodedImage, scaleFactor)
+	} else if programCommand == "merge" {
+		resultImage, encodeErr = mergeImage(decodedImage, expandedMergeFile)
+	}
+
+	if encodeErr != nil {
+		fmt.Println("Error encoding image: ", encodeErr.Error())
+		os.Exit(1)
+	} else if resultImage == nil {
+		fmt.Println("No image was encoded...")
+		os.Exit(1)
+	}
+
+	var outputFilePath string
+	var outputFileExtension string
+	if len(os.Args) >= 5 {
+		outputFilePath = os.Args[4]
+		regexpMatches = compiledRegex.FindSubmatch([]byte(outputFilePath))
+		if len(regexpMatches) > 1 {
+			outputFileExtension = string(regexpMatches[1])
+		}
+	}
+	var outputFile *os.File
+	outputFile, err = os.Open(outputFilePath)
+	if err != nil {
+		fmt.Println("Error opening file: ", err.Error())
+		os.Exit(1)
+	}
+	outputFileWriter := bufio.NewWriter(outputFile)
+
+	var encodingError error
+	if strings.ToLower(outputFileExtension) == "gif" {
+		fmt.Println("Encoding gif...")
+		encodingError = gif.Encode(outputFileWriter, resultImage, nil)
+	} else if strings.ToLower(outputFileExtension) == "jpeg" {
+		fmt.Println("Encoding jpeg...")
+		encodingError = jpeg.Encode(outputFileWriter, resultImage, nil)
+	} else if strings.ToLower(outputFileExtension) == "png" {
+		fmt.Println("Encoding png...")
+		encodingError = png.Encode(outputFileWriter, resultImage)
+	} else {
+		fmt.Println("Unknown file extension:", fileExtension)
+		fmt.Println("Not applying any encoding...")
+	}
+
+	if encodingError != nil {
+		fmt.Println("Error encoding file: ", encodingError.Error())
+	}
+}
+
+func scaleImage(image image.Image, scaleFactor int) (image.Image, error) {
+
+	fmt.Println("Scaling image...")
+	fmt.Println("ScaleFactor: ", scaleFactor)
+	return nil, nil
+}
+
+func mergeImage(image image.Image, mergeFilePath string) (image.Image, error) {
+
+	fmt.Println("Merging image...")
+	fmt.Println("Merge-file path: ", mergeFilePath)
+	return nil, nil
 }
